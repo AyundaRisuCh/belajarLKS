d436a04542cf57d4060a76c414cca5de34ba09ba
AyundaRisuCh <itswobsgt@gmail.com>
2025-07-19 20:06:18 +0700

    Update tampil.php

--- tampil.php
+++ tampil.php
@@ -1,5 +1,5 @@
 <?php
-$db = new mysqli('replica-db.cepq8smwuvdk.us-east-1.rds.amazonaws.com', 'root', 'admin123#', 'sigap_db');
+$db = new mysqli('replica-db.cb24ouuuwt7w.ap-southeast-2.rds.amazonaws.com', 'root', 'admin123#', 'sigap_db');
 
 if ($db->connect_error) {
     die("Koneksi database gagal: " . $db->connect_error);
